87e7a2dd34b8514d899a82ee3da82c4d69ea3e82
miglitopictures <miguelmddesign@gmail.com>
2026-06-30 08:57:23 -0300

    my presentation base

---
+++ slides.js
@@ -0,0 +1,138 @@
+
+
+const slides = [
+    {
+        title: "Bom dia :)",
+        keywords: [
+            "Tecnologia e Criação",
+            "TQSQSSPMNTCP",
+            "O Lore"
+        ]
+    },
+    {
+        title: "Ferramentas de Criação",
+        keywords: [
+            "Aprendizado",
+            "Expansão",
+            "Entrega",
+        ]
+    },
+    {
+        title: "Como Estética",
+        keywords: [
+            "Manual",
+            { text: "Compasso", images: ["compasso-1.png", "compasso-2.png", "compasso-3.png", "impressora-ruim.png"] },
+            { text: "Ponta do Curral", images: ["ponta-carbono.jpg", "ponta-procreate.jpg", "ponta-caderno.jpg", "ponta-paisagem.jpg"] },
+            "Digital",
+            { text: "Finalmente Eu", images: ["finally_me-1.png", "finally_me-2.png", "finally_me-3.png"] },
+            { text: "Histérica", images: ["histerica-fofa.png", "histerica-bruta.png", "histerica-servir.png"] },
+        ]
+    },
+    {
+        title: "Sistemas Visuais, Web, Padrões e Figma",
+        keywords: [
+            "Styleguides",
+            "Componentes",
+            "Relações",
+            "Consistência",
+        ]
+    },
+    {
+        title: "IA e criação de ferramentas",
+        keywords: [
+            "Barreira Técnica",
+            "p5js",
+            {
+                text: "Visu Haus",
+                images: [
+                    { image: "visu-haus-1.jpg", href: "https://visu.haus/" },
+                    "visu-haus-2.jpg"
+                ]
+            },
+            {
+                text: "Plate",
+                images: [
+                    { image: "plate-ui.jpg", href: "https://app.plate.video/" }
+                ]
+            }
+        ]
+    },
+    {
+        title: "Como Produto",
+        keywords: [
+            {
+                text: "Next Studio",
+                images: [
+                    { image: "converse-1.jpg", href: "https://nextstudio.vercel.app/tools/limitless" },
+                    "converse-2.jpg",
+                    "converse-3.jpg"
+                ]
+            },
+            {
+                text: "Guilt By Association",
+                images: [
+                    { image: "gba-rabisco.jpg", href: "https://gba-logo.netlify.app/" },
+                    "gba-pintura.jpg",
+                    "gba-neon.jpg"
+                ]
+            },
+            {
+                text: "BCG Image Gen",
+                images: [
+                    { image: "bcg-unreal-1.jpg", href: "https://someform.studio/BCG" },
+                    "bcg-unreal-2.jpg"
+                ]
+            },
+            {
+                text: "Deepmind 3D",
+                images: ["deepmind-1.jpg", "deepmind-2.jpg"]
+            }
+        ]
+    },
+    {
+        title: "Como Produto",
+        keywords: [
+            {
+                text: "Rio Carnaval",
+                images: [
+                    { image: "rio-carnaval-p5.jpg", href: "https://tatil.com.br/riocarnaval2022/" },
+                    "rio-carnaval-frame.jpg"
+                ]
+            },
+            {
+                text: "Enjoei",
+                images: ["enjoei-1.jpg", "enjoei-2.jpg", "enjoei-3.jpg"]
+            },
+            {
+                text: "Philharmonie Luxembourg",
+                images: ["philharmonie-1.jpg", "philharmonie-2.jpg"]
+            },
+            {
+                text: "CIDDIC",
+                images: ["philharmonie-1.jpg", "philharmonie-2.jpg"]
+            },
+            {
+                text: "Caracol",
+                images: ["caracol-1.jpg", "caracol-2.jpg", "caracol-3.jpg", "caracol-4.jpg", "caracol-5.jpg"]
+            },
+        ]
+    },
+    {
+        title: "Por Fim",
+        keywords: [
+            "Turbulência",
+            "Desafio",
+            "Projetistas",
+            "Potencial"
+        ]
+    },
+    {
+        title: "Obrigado! (╥﹏╥)",
+        keywords: [
+            "5 Anos",
+            "Diversão",
+            "Curiosidade",
+            "Orgulho"
+        ]
+    }
+];
